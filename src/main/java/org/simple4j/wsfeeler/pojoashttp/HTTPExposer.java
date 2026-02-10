858311d8be97df723c5cb0a1a21fb985fbb6e31c
Srinivas Janakiraman <jsrinivas@gmail.com>
2026-02-09 20:43:39 -0500

    added uniform marshaling of ZonedDateTime

--- src/main/java/org/simple4j/wsfeeler/pojoashttp/HTTPExposer.java
+++ src/main/java/org/simple4j/wsfeeler/pojoashttp/HTTPExposer.java
@@ -15,6 +15,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
@@ -28,6 +29,7 @@ public abstract class HTTPExposer
     private final ObjectMapper OBJECT_MAPPER = JsonMapper.builder()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             .serializationInclusion(Include.NON_NULL)
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
             .build();
 
     /**
